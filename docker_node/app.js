b71d8c3a89531aa332476eca59e0641ee6fcbe3a
Junhong.Qiu <496699722@qq.com>
2018-03-20 15:06:24 +0800

    nodeCountAccess

---
+++ docker_node/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var redis = require('redis');
+var app = express();
+
+var redisHost = process.env['REDIS_PORT_6379_TCP_ADDR'];
+var reditPort = process.env['REDIS_PORT_6379_TCP_PORT'];
+
+var redisClient = redis.createClient(reditPort, redisHost);
+
+app.get('/', function(req, res) {
+  console.log('get request');
+  redisClient.get('access_count', function(err, countNum) {
+    if(err) {
+      return res.send('get access count error');
+    }
+    if(!countNum) {
+      countNum = 1
+    }
+    else {
+      countNum = parseInt(countNum) + 1
+    }
+    redisClient.set('access_count', countNum, function(err) {
+      if(err) {
+        return res.send('set access count error')
+      }
+      res.send(countNum.toString())
+    })
+  })
+});
+
+app.listen(8000);
